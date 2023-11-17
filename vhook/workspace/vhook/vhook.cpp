41b3f53fb0adab4150983ed13f4656818a79edbc
roka <93293947+kernelenjoyer@users.noreply.github.com>
2023-11-17 06:46:09 +0100

    removed gay ass reinterpret_casting + fixed header typo

--- vhook/workspace/vhook/vhook.cpp
+++ vhook/workspace/vhook/vhook.cpp
@@ -2,7 +2,7 @@
 
 /*
 
-	vHook - simple module library.
+	vHook - the simplest dll hacking library.
 	made by stable / roka.
 
 */
@@ -36,49 +36,49 @@ auto vhook::open_console( ) -> void
 
 auto vhook::trampoline_hook( uintptr_t function , uintptr_t custom_function , size_t instance_size ) -> uintptr_t
 {
-    constexpr auto extra_size = 5;
+	constexpr auto extra_size = 5;
 
-    auto clone = reinterpret_cast< uintptr_t >( VirtualAlloc( nullptr , instance_size + extra_size , MEM_COMMIT | MEM_RESERVE , PAGE_EXECUTE_READWRITE ) );
-    if ( !clone ) return 0;
+	auto clone = ( uintptr_t ) ( VirtualAlloc( nullptr , instance_size + extra_size , MEM_COMMIT | MEM_RESERVE , PAGE_EXECUTE_READWRITE ) );
+	if ( !clone ) return 0;
 
-    memmove( reinterpret_cast< void* >( clone ) , reinterpret_cast< void* >( function ) , instance_size );
+	memmove( ( void* ) ( clone ) , ( void* ) ( function ) , instance_size );
 
-    *( uint8_t* ) ( clone + instance_size ) = 0xE9;
-    *( uintptr_t* ) ( clone + instance_size + 1 ) = ( function - clone - extra_size );
+	*( uint8_t* ) ( clone + instance_size ) = 0xE9;
+	*( uintptr_t* ) ( clone + instance_size + 1 ) = ( function - clone - extra_size );
 
-    DWORD old_protect { 0 };
+	DWORD old_protect { 0 };
 
-    if ( !VirtualProtect( reinterpret_cast< void* >( function ) , instance_size , PAGE_EXECUTE_READWRITE , &old_protect ) )
-    {
-        printf( "Could not change function's memory page protection." );
-        VirtualFree( reinterpret_cast< void* >( clone ) , 0 , MEM_RELEASE );
-        return 0;
-    }
+	if ( !VirtualProtect( ( void* ) ( function ) , instance_size , PAGE_EXECUTE_READWRITE , &old_protect ) )
+	{
+		printf( "Could not change function's memory page protection." );
+		VirtualFree( ( void* ) ( clone ) , 0 , MEM_RELEASE );
+		return 0;
+	}
 
-    memset( reinterpret_cast< void* >( function ) , 0x90 , instance_size );
+	memset( ( void* ) ( function ) , 0x90 , instance_size );
 
-    *( uint8_t* ) ( function ) = 0xE9;
-    *( uintptr_t* ) ( function + 1 ) = ( custom_function - function - extra_size );
+	*( uint8_t* ) ( function ) = 0xE9;
+	*( uintptr_t* ) ( function + 1 ) = ( custom_function - function - extra_size );
 
-    if ( !VirtualProtect( reinterpret_cast< void* >( function ) , instance_size , old_protect , &old_protect ) )
-    {
-        printf( "Could not restore the original protection of the function" );
-        VirtualFree( reinterpret_cast< void* >( clone ) , 0 , MEM_RELEASE );
-        return 0;
-    }
+	if ( !VirtualProtect( ( void* ) ( function ) , instance_size , old_protect , &old_protect ) )
+	{
+		printf( "Could not restore the original protection of the function" );
+		VirtualFree( ( void* ) ( clone ) , 0 , MEM_RELEASE );
+		return 0;
+	}
 
-    return clone;
+	return clone;
 }
 
 auto vhook::get_module_base( ) -> uintptr_t
 {
-	const ntos::peb* peb = reinterpret_cast< ntos::peb* >( __readgsqword( 0x60 ) );
+	const ntos::peb* peb = ( ntos::peb* ) ( __readgsqword( 0x60 ) );
 	if ( !peb ) return uintptr_t( 0 );
 
 	const ntos::list_entry* main_mod_entry = peb->Ldr->InMemoryOrderModuleList.Flink;
 	if ( main_mod_entry == &peb->Ldr->InMemoryOrderModuleList ) return uintptr_t( 0 );
 
-	const ntos::ldr_data_table_entry* main_mod = reinterpret_cast< ntos::ldr_data_table_entry* >( contains_record( main_mod_entry , ntos::ldr_data_table_entry , InMemoryOrderLinks ) );
+	const ntos::ldr_data_table_entry* main_mod = ( ntos::ldr_data_table_entry* ) ( contains_record( main_mod_entry , ntos::ldr_data_table_entry , InMemoryOrderLinks ) );
 	return std::uintptr_t( main_mod->DllBase );
 }
 
@@ -86,16 +86,16 @@ auto vhook::get_section_by_name( const char* sectionName ) -> SectionData
 {
 	HMODULE mod = ( HMODULE ) get_module_base( );
 
-	IMAGE_DOS_HEADER* dos_headers = reinterpret_cast< IMAGE_DOS_HEADER* >( mod );
-	IMAGE_NT_HEADERS* nt_headers = reinterpret_cast< IMAGE_NT_HEADERS* >( reinterpret_cast< BYTE* >( mod ) + dos_headers->e_lfanew );
+	IMAGE_DOS_HEADER* dos_headers = ( IMAGE_DOS_HEADER* ) ( mod );
+	IMAGE_NT_HEADERS* nt_headers = ( IMAGE_NT_HEADERS* ) ( ( BYTE* ) ( mod ) +dos_headers->e_lfanew );
 	IMAGE_SECTION_HEADER* section_header = IMAGE_FIRST_SECTION( nt_headers );
 
 	for ( int i = 0; i < nt_headers->FileHeader.NumberOfSections; ++i , ++section_header )
 	{
-		const char* curr_section_name = reinterpret_cast< const char* >( section_header->Name );
+		const char* curr_section_name = ( const char* ) ( section_header->Name );
 		if ( strcmp( curr_section_name , sectionName ) == 0 )
 		{
-			uintptr_t sectionStart = reinterpret_cast< uintptr_t >( mod ) + section_header->VirtualAddress;
+			uintptr_t sectionStart = ( uintptr_t ) ( mod ) +section_header->VirtualAddress;
 			size_t sectionSize = section_header->SizeOfRawData;
 			return { sectionStart, sectionSize };
 		}
@@ -108,7 +108,7 @@ auto vhook::iat_hook( const char* function_name , orig_test_func custom_func ) -
 {
 	HMODULE mod = ( HMODULE ) vhook::get_module_base( );
 
-	PIMAGE_DOS_HEADER dos_headers = ( PIMAGE_DOS_HEADER )( mod );
+	PIMAGE_DOS_HEADER dos_headers = ( PIMAGE_DOS_HEADER ) ( mod );
 	PIMAGE_NT_HEADERS nt_headers = ( PIMAGE_NT_HEADERS ) ( ( BYTE* ) ( mod ) +dos_headers->e_lfanew );
 	PIMAGE_IMPORT_DESCRIPTOR import_descriptor = ( PIMAGE_IMPORT_DESCRIPTOR ) ( ( BYTE* ) ( mod ) +nt_headers->OptionalHeader.DataDirectory[ IMAGE_DIRECTORY_ENTRY_IMPORT ].VirtualAddress );
 
@@ -122,7 +122,7 @@ auto vhook::iat_hook( const char* function_name , orig_test_func custom_func ) -
 		{
 			PIMAGE_IMPORT_BY_NAME import_by_dawg = ( PIMAGE_IMPORT_BY_NAME ) ( ( BYTE* ) ( mod ) +thunk_data->u1.Function );
 
-			if ( strcmp( reinterpret_cast< char* >( import_by_dawg->Name ) , function_name ) == 0 )
+			if ( strcmp( ( char* ) ( import_by_dawg->Name ) , function_name ) == 0 )
 			{
 				DWORD oldProtect;
 				VirtualProtect( &thunk_data->u1.Function , sizeof( PVOID ) , PAGE_READWRITE , &oldProtect );
